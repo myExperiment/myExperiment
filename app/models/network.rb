fba8704b3ad6b793215ce64f1aeb84c4b62c2afd
Mark Borkum <mib104@soton.ac.uk>
2007-07-30 23:08:26 +0000

    fixed relations and foaf methods
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/m2@342 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- app/models/network.rb
+++ app/models/network.rb
@@ -11,25 +11,25 @@ class Network < ActiveRecord::Base
   
   has_many :relationships
   
-  has_and_belongs_to_many :relations_of_mine,
+  # SELF -- child_relation --> relation_id
+  # * child = relation_id
+  has_and_belongs_to_many :relations,
                           :class_name => "Network",
                           :join_table => :relationships,
                           :foreign_key => :network_id,
                           :association_foreign_key => :relation_id,
                           :conditions => ["accepted_at < ?", Time.now],
                           :order => "accepted_at DESC"
-                          
-  has_and_belongs_to_many :relations_with_me,
-                          :class_name => "Network",
-                          :join_table => :relationships,
-                          :foreign_key => :relation_id,
-                          :association_foreign_key => :network_id,
-                          :conditions => ["accepted_at < ?", Time.now],
-                          :order => "accepted_at DESC"
-                          
-  def relations
-    (self.relations_of_mine + self.relations_with_me).uniq
-  end
+  
+  # network_id -- parent_relation --> SELF
+  # * parent = network_id
+  # has_and_belongs_to_many :parent_relations,
+  #                         :class_name => "Network",
+  #                         :join_table => :relationships,
+  #                         :foreign_key => :relation_id,
+  #                         :association_foreign_key => :network_id,
+  #                         :conditions => ["accepted_at < ?", Time.now],
+  #                         :order => "accepted_at DESC"
                           
   has_many :memberships
   
@@ -40,4 +40,64 @@ class Network < ActiveRecord::Base
                           :association_foreign_key => :network_id,
                           :conditions => ["accepted_at < ?", Time.now],
                           :order => "accepted_at DESC"
+                          
+  def member?(user_id, recursive = false)
+    if (rel = self.relations).empty? or !recursive
+      self.members.include? User.find(user_id)
+    else
+      rel.each do |r|
+        return true if Network.find(r.relation_id).member? user_id, true
+      end
+      
+      false
+    end
+  end
+  
+  def relation?(network_id, recursive = false)
+    if (rel = self.relations).empty? or !recursive
+      n = Network.find(network_id).id
+      
+      rel.each do |r|
+        return true if r.relation_id.to_i == n.to_i
+      end
+      
+      false
+    else
+      return true if self.relation? network_id, false
+      
+      rel.each do |r|
+        return true if Network.find(r.relation_id).relation? network_id, true
+      end
+        
+      false
+    end
+  end
+  
+  def rmembers
+    rtn = self.members
+    
+    self.relations.each do |r|
+      rtn = (rtn + Network.find(r.relation_id).rmembers)
+    end
+    
+    rtn.uniq
+  end
+  
+  def members_recursive 
+    rmembers
+  end
+  
+  def rrelations
+    rtn = self.relations
+    
+    self.relations.each do |r|
+      rtn = (rtn + Network.find(r.relation_id).rrelations)
+    end
+    
+    rtn # no need for uniq (there shouldn't be any loops)
+  end
+  
+  def relations_recursive 
+    rrelations
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,26 +8,26 @@ class User < ActiveRecord::Base
   has_many :pictures
   
   has_many :friendships
-          
-  has_and_belongs_to_many :friends_of_mine,
+
+  # SELF -- friend --> friend_id
+  # * friend = friend_id
+  has_and_belongs_to_many :friends,
                           :class_name => "User", 
                           :join_table => :friendships,
                           :foreign_key => :user_id,
                           :association_foreign_key => :friend_id,
                           :conditions => ["accepted_at < ?", Time.now],
                           :order => "accepted_at DESC"
-                          
-  has_and_belongs_to_many :friends_with_me,
-                          :class_name => "User", 
-                          :join_table => :friendships,
-                          :foreign_key => :friend_id,
-                          :association_foreign_key => :user_id,
-                          :conditions => ["accepted_at < ?", Time.now],
-                          :order => "accepted_at DESC"
-                          
-  def friends
-    (self.friends_of_mine + self.friends_with_me).uniq
-  end
+                            
+  # user_id -- friend_with_me --> SELF
+  # * friend = user_id
+  # has_and_belongs_to_many :friends_with_me,
+  #                         :class_name => "User", 
+  #                         :join_table => :friendships,
+  #                         :foreign_key => :friend_id,
+  #                         :association_foreign_key => :user_id,
+  #                         :conditions => ["accepted_at < ?", Time.now],
+  #                         :order => "accepted_at DESC"
                           
   has_many :memberships
                           
@@ -40,4 +40,18 @@ class User < ActiveRecord::Base
                           
   has_many :networks_owned,
            :class_name => "Network"
+           
+  def foaf(user_id, depth=0, maxdepth=7)
+    unless depth > maxdepth
+      (fri = self.friends).each do |f|
+        return true if f.friend_id.to_i == user_id.to_i
+      end
+      
+      fri.each do |f|
+        return true if User.find(f.friend_id).foaf user_id, depth+1, maxdepth
+      end
+    end
+    
+    false
+  end
 end
