36fbb538e870add176187fef271e9f04fa493a59
Mark Borkum <mib104@soton.ac.uk>
2007-08-09 16:26:20 +0000

    added (basic) RESTful login/logout actions for auth_controller.rb
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/m2@394 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,21 +1,57 @@
 class AuthController < ApplicationController
-  def login(user_id = 1)
-    flash[:notice] = "#{User.find(user_id).name} has logged in."
-    session[:user_id] = user_id
-    
-    respond_to do |format|
-      format.html { redirect_to :controller => 'users' }
-      format.xml { head :ok }
+  def login
+    begin
+      u = User.find(params[:id])
+      
+      if logged_in?
+        if current_user.id.to_i == u.id.to_i
+          error("Login failed, already logged in", "already logged in")
+        else
+          error("Login failed, please log out first", "log out first")
+        end
+      else
+        session[:user_id] = u.id
+        flash[:notice] = "#{u.name} has logged in."
+          
+        respond_to do |format|
+          format.html { redirect_to users_url }
+          format.xml { head :ok }
+        end
+      end
+    rescue ActiveRecord::RecordNotFound
+      error("Login failed, user not found (id invalid)", "not found (id invalid)")
     end
   end
   
-  def logout(user_id = 1)
-    flash[:notice] = "#{User.find(user_id).name} has logged out."
-    session[:user_id] = nil
+  def logout
+    begin
+      u = User.find(params[:id])
       
+      if logged_in? and current_user.id.to_i == u.id.to_i
+        flash[:notice] = "#{u.name} has logged out."
+        session[:user_id] = nil
+      
+        respond_to do |format|
+          format.html { redirect_to users_url }
+          format.xml { head :ok }
+        end
+      else
+        error("Logout failed, not logged in", "not logged in")
+      end
+    rescue ActiveRecord::RecordNotFound
+      error("Logout failed, user not found (id invalid)", "not found (id invalid)")
+    end
+  end
+  
+private
+
+  def error(notice, message, attr=:id)
+    flash[:notice] = notice
+    (err = User.new.errors).add(attr, message)
+    
     respond_to do |format|
-      format.html { redirect_to :controller => 'users' }
-      format.xml { head :ok }
+      format.html { redirect_to users_url }
+      format.xml { render :xml => err.to_xml }
     end
   end
 end
