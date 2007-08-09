b12bd015474e91c78c98139cb222f551da4abfce
Mark Borkum <mib104@soton.ac.uk>
2007-08-09 14:23:59 +0000

    added RESTful blobs
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/m2@390 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- app/controllers/blobs_controller.rb
+++ app/controllers/blobs_controller.rb
@@ -1,8 +1,12 @@
 class BlobsController < ApplicationController
+  before_filter :authorize, :except => [:index, :show, :download]
+  
+  before_filter :find_blobs, :only => [:index]
+  before_filter :find_blob, :only => [:show]
+  before_filter :find_blob_auth, :only => [:download, :edit, :update, :destroy]
+  
   # GET /blobs/1;download
   def download
-    @blob = Blob.find(params[:id])
-    
     if @blob.authorized?("download", (logged_in? ? current_user : nil))
       send_data(@blob.data, :filename => @blob.local_name, :type => @blob.content_type)
     else
@@ -16,8 +20,6 @@ class BlobsController < ApplicationController
   # GET /blobs
   # GET /blobs.xml
   def index
-    @blobs = Blob.find(:all)
-
     respond_to do |format|
       format.html # index.rhtml
       format.xml  { render :xml => @blobs.to_xml }
@@ -27,8 +29,6 @@ class BlobsController < ApplicationController
   # GET /blobs/1
   # GET /blobs/1.xml
   def show
-    @blob = Blob.find(params[:id])
-
     respond_to do |format|
       format.html # show.rhtml
       format.xml  { render :xml => @blob.to_xml }
@@ -42,22 +42,15 @@ class BlobsController < ApplicationController
 
   # GET /blobs/1;edit
   def edit
-    @blob = Blob.find(params[:id])
+
   end
 
   # POST /blobs
   # POST /blobs.xml
   def create
-    params[:blob][:local_name] = sanitize_filename(params[:blob][:data].original_filename)
+    params[:blob][:local_name] = params[:blob][:data].original_filename
     params[:blob][:content_type] = params[:blob][:data].content_type
     params[:blob][:data] = params[:blob][:data].read
-    
-    # params[:blob][:data].rewind
-    # File.open("#{RAILS_ROOT}/uploaded_#{controller_name}/#{params[:blob][:contributor_type].downcase.pluralize}/#{params[:blob][:contributor_id]}/#{params[:blob][:local_name]}", "wb") do |f|  
-    #   f.write(params[:blob][:data].read)
-    # end
-    
-    # params[:blob].delete('data')
     
     @blob = Blob.new(params[:blob])
 
@@ -76,8 +69,6 @@ class BlobsController < ApplicationController
   # PUT /blobs/1
   # PUT /blobs/1.xml
   def update
-    @blob = Blob.find(params[:id])
-
     respond_to do |format|
       if @blob.update_attributes(params[:blob])
         flash[:notice] = 'Blob was successfully updated.'
@@ -93,7 +84,6 @@ class BlobsController < ApplicationController
   # DELETE /blobs/1
   # DELETE /blobs/1.xml
   def destroy
-    @blob = Blob.find(params[:id])
     @blob.destroy
 
     respond_to do |format|
@@ -102,13 +92,43 @@ class BlobsController < ApplicationController
     end
   end
   
+protected
+
+  def find_blobs
+    @blobs = Blob.find(:all, :order => "local_name ASC")
+  end
+  
+  def find_blob
+    begin
+      @blob = Blob.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      error("Blob not found", "is invalid")
+    end
+  end
+  
+  def find_blob_auth
+    begin
+      blob = Blob.find(params[:id])
+      
+      if blob.authorized?(action_name, (logged_in? ? current_user : nil))
+        @blob = blob
+      else
+        error("Blob not found (id not authorized)", "is invalid (not authorized)")
+      end
+    rescue ActiveRecord::RecordNotFound
+      error("Blob not found", "is invalid")
+    end
+  end
+  
 private
 
-  # http://manuals.rubyonrails.com/read/chapter/78
-  def sanitize_filename(file_name)
-    # get only the filename, not the whole path (from IE)
-    just_filename = File.basename(file_name) 
-    # replace all non-alphanumeric, underscore or periods with underscores
-    just_filename.gsub(/[^\w\.\-]/,'_') 
+  def error(notice, message, attr=:id)
+    flash[:notice] = notice
+    (err = Blob.new.errors).add(attr, message)
+    
+    respond_to do |format|
+      format.html { redirect_to blobs_url }
+      format.xml { render :xml => err.to_xml }
+    end
   end
 end
