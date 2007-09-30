87dccc8f2d85973c0d905dda579510e6989fd497
Mark Borkum <mib104@soton.ac.uk>
2007-09-30 21:43:29 +0000

    minor edits
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/branches/mark-branch@663 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- app/models/policy.rb
+++ app/models/policy.rb
@@ -76,8 +76,8 @@ private
   # xxx_public and xxx_protected column in ++policies++ and an xxx 
   # column in ++permissions+)
   @@categories = { "download" => ["download"], 
-                   "edit" => ["new", "create", "edit", "update"], 
-                   "view" => ["index", "show", "tag", "search", "bookmark", "comment", "rate"],
+                   "edit" => ["new", "create", "edit", "update", "tag"], 
+                   "view" => ["index", "show", "search", "bookmark", "comment", "rate"],
                    "owner" => ["destroy"] } # you don't need a boolean column for this but you do need to categorize 'owner only' actions!
   
   # the policy class contains a hash table of action (method) names and their categories
