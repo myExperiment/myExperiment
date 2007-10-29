56863b86101454f0bdeda75c6f6a0fe1035a677a
Jiten Bhagat <jits@jits.co.uk>
2007-10-29 15:30:07 +0000

    Moved HTML 'cleaner' method to application.rb controller + other minor changes
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/branches/mark-branch@842 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- app/controllers/workflows_controller.rb
+++ app/controllers/workflows_controller.rb
@@ -318,38 +318,6 @@ protected
     
     return rtn
   end
-  
-  # Safe HTML - http://www.anyexample.com/webdev/rails/how_to_allow_some_safe_html_in_rails_projects.xml
-  def ae_some_html(s)
-    # converting newlines
-    s.gsub!(/\r\n?/, "\n")
- 
-    # escaping HTML to entities
-    s = s.to_s.gsub('&', '&amp;').gsub('<', '&lt;').gsub('>', '&gt;')
- 
-    # blockquote tag support
-    s.gsub!(/\n?&lt;blockquote&gt;\n*(.+?)\n*&lt;\/blockquote&gt;/im, "<blockquote>\\1</blockquote>")
- 
-    # other tags: b, i, em, strong, u
-    %w(b i em strong u).each { |x|
-         s.gsub!(Regexp.new('&lt;(' + x + ')&gt;(.+?)&lt;/('+x+')&gt;',
-                 Regexp::MULTILINE|Regexp::IGNORECASE), 
-                 "<\\1>\\2</\\1>")
-        }
- 
-    # A tag support
-    # href="" attribute auto-adds http://
-    s = s.gsub(/&lt;a.+?href\s*=\s*['"](.+?)["'].*?&gt;(.+?)&lt;\/a&gt;/im) { |x|
-            '<a href="' + ($1.index('://') ? $1 : 'http://'+$1) + "\">" + $2 + "</a>"
-          }
- 
-    # replacing newlines to <br> ans <p> tags
-    # wrapping text into paragraph
-    s = "<p>" + s.gsub(/\n\n+/, "</p>\n\n<p>").
-            gsub(/([^\n]\n)(?=[^\n])/, '\1<br />') + "</p>"
- 
-    s      
-  end
 
 private
 
