d5b8783e9f7100e83adc95c6def677cbdbf04874
Jiten Bhagat <jits@jits.co.uk>
2007-11-09 13:08:25 +0000

    Mailer now sends email to bugs@myexperiment.org + changes on feedback page
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/branches/mark-branch@938 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -2,7 +2,8 @@ class Mailer < ActionMailer::Base
 
   def feedback(name, subject, content)
     #recipients "dgc@ecs.soton.ac.uk"
-    recipients "jits@cs.man.ac.uk"
+    #recipients "jits@cs.man.ac.uk"
+    recipients "bugs@myexperiment.org"
     from "myexpmail@gmail.com"
     subject "myExperiment feedback from #{name}"
     body :name => name, :subject => subject, :content => content
