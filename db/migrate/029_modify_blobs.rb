cf8e7c7120c3412650abf82452045695852cf478
Don Cruickshank <dgc@ecs.soton.ac.uk>
2007-11-12 10:28:40 +0000

    added title and description to blobs
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/branches/mark-branch@948 4f7b4649-9ca9-420a-81b3-55a188e4d46f

---
+++ db/migrate/029_modify_blobs.rb
@@ -0,0 +1,12 @@
+
+class ModifyBlobs < ActiveRecord::Migration
+  def self.up
+    add_column :blobs, :title, :string
+    add_column :blobs, :description, :text
+  end
+
+  def self.down
+    remove_column :blobs, :title
+    remove_column :blobs, :description
+  end
+end
