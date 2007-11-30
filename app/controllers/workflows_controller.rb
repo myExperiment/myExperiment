83b2b0f52ac4241c472b58a7c56b6bc3790a679e
Don Cruickshank <dgc@ecs.soton.ac.uk>
2007-11-30 15:55:25 +0000

    switched licenses to BSD
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/branches/mark-branch@1077 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- app/controllers/workflows_controller.rb
+++ app/controllers/workflows_controller.rb
@@ -1,25 +1,7 @@
-##
-##
-## myExperiment - a social network for scientists
-##
-## Copyright (C) 2007 University of Manchester/University of Southampton
-##
-## This program is free software; you can redistribute it and/or modify
-## it under the terms of the GNU Affero General Public License
-## as published by the Free Software Foundation; either version 3 of the
-## License, or (at your option) any later version.
-##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU Affero General Public License for more details.
-##
-## You should have received a copy of the GNU Affero General Public License
-## along with this program; if not, see http://www.gnu.org/licenses
-## or write to the Free Software Foundation,Inc., 51 Franklin Street,
-## Fifth Floor, Boston, MA 02110-1301  USA
-##
-##
+# myExperiment: app/controllers/workflows_controller.rb
+#
+# Copyright (c) 2007 University of Manchester and the University of Southampton.
+# See license.txt for details.
 
 class WorkflowsController < ApplicationController
   before_filter :login_required, :except => [:index, :show, :download, :named_download, :search, :all]
