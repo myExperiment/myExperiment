105cdc1a3b79b689a3b5024952427a9ae5fa336a
Jiten Bhagat <jits@jits.co.uk>
2008-01-02 18:39:56 +0000

    Users can now log in with username OR email.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/branches/mark-branch@1104 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- app/models/user.rb
+++ app/models/user.rb
@@ -115,18 +115,17 @@ class User < ActiveRecord::Base
     end
   end
   
-  # Authenticates a user by their login name and unencrypted password.  Returns the user or nil.
-  def self.authenticate(username, password)
-    u = find(:first, :conditions => ["username = ?", username]) # need to get the salt
+  # Authenticates a user by their login name OR email and unencrypted password.  Returns the user or nil.
+  def self.authenticate(login, password)
+    return nil if login.blank? or password.blank?
     
-    # use email address as RESTful username
-    # unless u
-    #   u = find(:first, 
-    #            :joins => "LEFT JOIN profiles ON profiles.user_id = users.id",
-    #            :conditions => ["profiles.email = ?", username]) 
-    #           
-    #   u = find(:first, :conditions => ["id = ?", u.id])
-    # end
+    # Either, check for a User with username matching 'login'
+    u = find(:first, :conditions => ["username = ?", login])
+    
+    # Or, check for a User with email address matching 'login'
+    unless u
+      u = find(:first, :conditions => ["email = ?", login]) 
+    end
     
     u && u.authenticated?(password) ? u : nil
   end
