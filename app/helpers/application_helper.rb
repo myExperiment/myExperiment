9cb872fcba68d0fc0bcbb0c814c5a6772b4a1d95
Jiten Bhagat <jits@jits.co.uk>
2008-01-22 17:07:47 +0000

    Changes for new 'Home' tab.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/branches/mark-branch@1159 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -105,7 +105,7 @@ module ApplicationHelper
     
     name = truncate_to ? truncate(user.name, truncate_to) : name = user.name
     
-    return link_to(h(name), user_url(user), :title => tooltip_title_attrib(user.name))
+    return link_to(h(name), user_url(user), :title => tooltip_title_attrib(h(user.name)))
   end
   
   def title(network_id, truncate_to=nil)
