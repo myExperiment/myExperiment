7d69b9ae37d575146767cc7f33df55739478293e
Jiten Bhagat <jits@jits.co.uk>
2008-02-27 23:22:38 +0000

    Back to Rails 1.2.6
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/trunk@1287 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '1.2.3' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '1.2.6' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -19,7 +19,7 @@ Rails::Initializer.run do |config|
   # Only load the plugins named here, by default all plugins in vendor/plugins are loaded
   # config.plugins = %W( exception_notification ssl_requirement )
 
-  config.plugins = ["engines", "encrypted_strings", "*"]
+  config.plugins = ["engines", "encrypted_strings", "widgets", "*"]
 
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
