2064e26164eda26c5b52645f0d21662649464871
Jiten Bhagat <jits@jits.co.uk>
2008-03-04 16:18:57 +0000

    Attempt at rendering images for the outputs viewer, but no luck.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/trunk@1312 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -10,38 +10,20 @@ module JobsHelper
       case o_type
       when "list"
         if data.value.size == 1
-          result += get_content(data.value, m_types)
+          result += get_content(data.value, m_types, job.id)
         else
           result += "<ol class=\"outputs_list\">\n"
-          result += build_nested_list(data.value, m_types)
+          result += build_nested_list(data.value, m_types, job.id)
           result += "</ol>\n"
         end
       when "string"
-        result += get_content(data.value, m_types)
+        result += get_content(data.value, m_types, job.id)
       else
-        result += get_content(data.value, m_types)
+        result += get_content(data.value, m_types, job.id)
       end
     end
     
     return result
-  end
-  
-  def get_value(job, key, data, metadata, index)
-    if metadata
-      for type in metadata do
-        if type.index 'image'
-           image_path = "jobs/#{job.id.to_s}/#{key}"
-           image = "#{index.to_s}.img"
-           FileUtils.mkpath("public/#{image_path}")
-           file = File.new("public/#{image_path}/#{image}", 'w+')
-           file.write(Base64.decode64(data))
-           file.close
-           return image_tag("/#{image_path}/#{image}")
-          return 'IMAGE'
-        end
-      end
-    end
-    Base64.decode64(data)
   end
   
   def mime_types_snippet(types)
@@ -50,32 +32,43 @@ module JobsHelper
   
   private
   
-  def build_nested_list(data_array, m_types)
+  def build_nested_list(data_array, m_types, job_id)
     result = ''
     data_array.each do |v|
       if v.is_a?(Array)
         result += "<li><b>List:</b></li>\n"
         result += "<ol>\n"
-        result += build_nested_list(v, m_types)
+        result += build_nested_list(v, m_types, job_id)
         result += "</ol>\n"
       elsif v.is_a?(String)
-        result += "<li>#{get_content(v, m_types)}</li>\n"
+        result += "<li>#{get_content(v, m_types, job_id)}</li>\n"
       else
-        result += "<li>#{get_content(v, m_types)}</li>\n"
+        result += "<li>#{get_content(v, m_types, job_id)}</li>\n"
       end
     end
     return result
   end
   
-  def get_content(data, m_types)
+  def get_content(data, m_types, job_id)
     m = m_types.to_sentence.downcase
     if m =~ /image/
-      return "This is an image!"
+      #return get_image(data, job_id)
+      return "[Image viewing is currently not supported]"
     elsif m =~ /html/
       return data.to_s
     else
       return "<pre>#{h data.to_s}</pre>"
     end
+  end
+  
+  def get_image(data, job_id)
+    image_path = "jobs/#{job_id.to_s}"
+    image = "#{rand(32768).to_s}.#{data.to_s[0, 3]}"
+    FileUtils.mkpath("public/#{image_path}")
+    file = File.new("public/#{image_path}/#{image}", 'w+')
+    file.write(data)
+    file.close
+    return image_tag("/#{image_path}/#{image}")
   end
   
 end
