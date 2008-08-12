9f0257c459726f43bdcb0e6e0e158afd0b8fe512
Sergejs Aleksejevs <alekses6@cs.man.ac.uk>
2008-08-12 13:32:22 +0000

    Refactored code in the invite page for networks - some logic moved out of the view to the helper.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/trunk@1697 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- app/helpers/networks_helper.rb
+++ app/helpers/networks_helper.rb
@@ -4,4 +4,15 @@
 # See license.txt for details.
 
 module NetworksHelper
+  
+  # chooses, which of the DIVs will be selected on the invite page, based
+  # on the value of a paramater
+  def invite_existing_selected?(radio_choice)
+    if radio_choice.nil?
+      return( true )
+    else
+      return( radio_choice == "existing" ? true : false )  
+    end
+  end
+  
 end
