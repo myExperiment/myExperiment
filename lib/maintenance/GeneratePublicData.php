1c931ec7d3279b92f29ed87fc43709eb51227077
David Newman <drn@ecs.soton.ac.uk>
2008-10-31 13:28:51 +0000

    Updated GeneratePublicData.php to fix bugs and to remove data from being generated for taverna_enactors and remote_workflows
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/trunk@1911 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- lib/maintenance/GeneratePublicData.php
+++ lib/maintenance/GeneratePublicData.php
@@ -16,6 +16,335 @@
 	$createhashes['viewings']=2;
 
 	//Create Table Statements
+	$createtable['announcements']="CREATE TABLE announcements (
+  id int(11) NOT NULL auto_increment,
+  title varchar(255) default NULL,
+  user_id int(11) default NULL,
+  created_at datetime default NULL,
+  updated_at datetime default NULL,
+  body text,
+  body_html text,
+  PRIMARY KEY  (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+	$createtable['attributions']="CREATE TABLE attributions (
+  id int(11) NOT NULL auto_increment,
+  attributor_id int(11) default NULL,
+  attributor_type varchar(255) default NULL,
+  attributable_id int(11) default NULL,
+  attributable_type varchar(255) default NULL,
+  created_at datetime default NULL,
+  updated_at datetime default NULL,
+  PRIMARY KEY  (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+	$createtable['blobs']="CREATE TABLE blobs (
+  id int(11) NOT NULL auto_increment,
+  contributor_id int(11) default NULL,
+  contributor_type varchar(255) default NULL,
+  local_name varchar(255) default NULL,
+  content_type varchar(255) default NULL,
+  created_at datetime default NULL,
+  updated_at datetime default NULL,
+  title varchar(255) default NULL,
+  body text,
+  license varchar(10) NOT NULL default 'by-nd',
+  body_html text,
+  content_blob_id int(11) default NULL,
+  PRIMARY KEY  (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+	$createtable['bookmarks']="CREATE TABLE bookmarks (
+  id int(11) NOT NULL auto_increment,
+  title varchar(50) default '',
+  created_at datetime NOT NULL default '0000-00-00 00:00:00',
+  bookmarkable_type varchar(15) NOT NULL default '',
+  bookmarkable_id int(11) NOT NULL default '0',
+  user_id int(11) NOT NULL default '0',
+  PRIMARY KEY  (id),
+  KEY fk_bookmarks_user (user_id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+	$createtable['citations']="CREATE TABLE citations (
+  id int(11) NOT NULL auto_increment,
+  user_id int(11) default NULL,
+  workflow_id int(11) default NULL,
+  workflow_version int(11) default NULL,
+  authors text,
+  title varchar(255) default NULL,
+  publication varchar(255) default NULL,
+  published_at datetime default NULL,
+  accessed_at datetime default NULL,
+  url varchar(255) default NULL,
+  isbn varchar(255) default NULL,
+  issn varchar(255) default NULL,
+  created_at datetime default NULL,
+  updated_at datetime default NULL,
+  PRIMARY KEY  (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+	$createtable['comments']="CREATE TABLE comments (
+  id int(11) NOT NULL auto_increment,
+  title varchar(50) default '',
+  `comment` text,
+  created_at datetime NOT NULL default '0000-00-00 00:00:00',
+  commentable_id int(11) NOT NULL default '0',
+  commentable_type varchar(15) NOT NULL default '',
+  user_id int(11) NOT NULL default '0',
+  PRIMARY KEY  (id),
+  KEY fk_comments_user (user_id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+	$createtable['contributions']="
+CREATE TABLE contributions (
+  id int(11) NOT NULL auto_increment,
+  contributor_id int(11) default NULL,
+  contributor_type varchar(255) default NULL,
+  contributable_id int(11) default NULL,
+  contributable_type varchar(255) default NULL,
+  policy_id int(11) default NULL,
+  created_at datetime default NULL,
+  updated_at datetime default NULL,
+  downloads_count int(11) default '0',
+  viewings_count int(11) default '0',
+  PRIMARY KEY  (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+	$createtable['creditations']="CREATE TABLE creditations (
+  id int(11) NOT NULL auto_increment,
+  creditor_id int(11) default NULL,
+  creditor_type varchar(255) default NULL,
+  creditable_id int(11) default NULL,
+  creditable_type varchar(255) default NULL,
+  created_at datetime default NULL,
+  updated_at datetime default NULL,
+  PRIMARY KEY  (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+	$createtable['downloads']="CREATE TABLE downloads (
+  id int(11) NOT NULL auto_increment,
+  contribution_id int(11) default NULL,
+  user_id char(32) default NULL,
+  created_at datetime default NULL,
+  PRIMARY KEY  (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+	$createtable['experiments']="CREATE TABLE experiments (
+  id int(11) NOT NULL auto_increment,
+  title varchar(255) default NULL,
+  description text,
+  description_html text,
+  contributor_id int(11) default NULL,
+  contributor_type varchar(255) default NULL,
+  created_at datetime default NULL,
+  updated_at datetime default NULL,
+  PRIMARY KEY  (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+	$createtable['friendships']="CREATE TABLE friendships (
+  id int(11) NOT NULL auto_increment,
+  user_id int(11) default NULL,
+  friend_id int(11) default NULL,
+  created_at datetime default NULL,
+  accepted_at datetime default NULL,
+  message varchar(500) default NULL,
+  PRIMARY KEY  (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+	$createtable['group_announcements']="CREATE TABLE group_announcements (
+  id int(11) NOT NULL auto_increment,
+  title varchar(255) default NULL,
+  network_id int(11) default NULL,
+  user_id int(11) default NULL,
+  public tinyint(1) default '0',
+  created_at datetime default NULL,
+  updated_at datetime default NULL,
+  body text,
+  body_html text,
+  PRIMARY KEY  (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+	$createtable['jobs']="CREATE TABLE jobs (
+  id int(11) NOT NULL auto_increment,
+  title varchar(255) default NULL,
+  description text,
+  description_html text,
+  experiment_id int(11) default NULL,
+  user_id int(11) default NULL,
+  runnable_id int(11) default NULL,
+  runnable_version int(11) default NULL,
+  runnable_type varchar(255) default NULL,
+  runner_id int(11) default NULL,
+  runner_type varchar(255) default NULL,
+  submitted_at datetime default NULL,
+  started_at datetime default NULL,
+  completed_at datetime default NULL,
+  last_status varchar(255) default NULL,
+  last_status_at datetime default NULL,
+  job_uri varchar(255) default NULL,
+  job_manifest longblob,
+  inputs_uri varchar(255) default NULL,
+  inputs_data longblob,
+  outputs_uri varchar(255) default NULL,
+  created_at datetime default NULL,
+  updated_at datetime default NULL,
+  parent_job_id int(11) default NULL,
+  PRIMARY KEY  (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+	$createtable['memberships']="CREATE TABLE memberships (
+  id int(11) NOT NULL auto_increment,
+  user_id int(11) default NULL,
+  network_id int(11) default NULL,
+  created_at datetime default NULL,
+  user_established_at datetime default NULL,
+  network_established_at datetime default NULL,
+  message varchar(500) default NULL,
+  PRIMARY KEY  (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+	$createtable['messages']="CREATE TABLE messages (
+  id int(11) NOT NULL auto_increment,
+  `from` int(11) default NULL,
+  `to` int(11) default NULL,
+  `subject` varchar(255) default NULL,
+  body text,
+  reply_id int(11) default NULL,
+  created_at datetime default NULL,
+  read_at datetime default NULL,
+  body_html text,
+  deleted_by_sender tinyint(1) default '0',
+  deleted_by_recipient tinyint(1) default '0',
+  PRIMARY KEY  (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+	$createtable['networks']="CREATE TABLE networks (
+  id int(11) NOT NULL auto_increment,
+  user_id int(11) default NULL,
+  title varchar(255) default NULL,
+  unique_name varchar(255) default NULL,
+  created_at datetime default NULL,
+  updated_at datetime default NULL,
+  description text,
+  description_html text,
+  auto_accept tinyint(1) default '0',
+  PRIMARY KEY  (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+
+	$createtable['pack_contributable_entries']="
+CREATE TABLE pack_contributable_entries (
+  id int(11) NOT NULL auto_increment,
+  pack_id int(11) NOT NULL,
+  contributable_id int(11) NOT NULL,
+  contributable_version int(11) default NULL,
+  contributable_type varchar(255) NOT NULL,
+  `comment` text,
+  user_id int(11) NOT NULL,
+  created_at datetime default NULL,
+  updated_at datetime default NULL,
+  PRIMARY KEY  (id)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;";
+	$createtable['pack_remote_entries']="
+CREATE TABLE pack_remote_entries (
+  id int(11) NOT NULL auto_increment,
+  pack_id int(11) NOT NULL,
+  title varchar(255) NOT NULL,
+  uri varchar(255) NOT NULL,
+  alternate_uri varchar(255) default NULL,
+  `comment` text,
+  user_id int(11) NOT NULL,
+  created_at datetime default NULL,
+  updated_at datetime default NULL,
+  PRIMARY KEY  (id)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;";
+	$createtable['packs']="CREATE TABLE packs (
+  id int(11) NOT NULL auto_increment,
+  contributor_id int(11) default NULL,
+  contributor_type varchar(255) default NULL,
+  title varchar(255) default NULL,
+  description text,
+  description_html text,
+  created_at datetime default NULL,
+  updated_at datetime default NULL,
+  PRIMARY KEY  (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+	$createtable['pending_invitations']="
+CREATE TABLE pending_invitations (
+  id int(11) NOT NULL auto_increment,
+  email varchar(255) default NULL,
+  created_at datetime default NULL,
+  request_type varchar(255) default NULL,
+  requested_by int(11) default NULL,
+  request_for int(11) default NULL,
+  message varchar(500) default NULL,
+  token varchar(255) default NULL,
+  PRIMARY KEY  (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+	$createtable['permissions']="
+CREATE TABLE permissions (
+  id int(11) NOT NULL auto_increment,
+  contributor_id int(11) default NULL,
+  contributor_type varchar(255) default NULL,
+  policy_id int(11) default NULL,
+  download tinyint(1) default '0',
+  edit tinyint(1) default '0',
+  `view` tinyint(1) default '0',
+  created_at datetime default NULL,
+  updated_at datetime default NULL,
+  PRIMARY KEY  (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+	$createtable['picture_selections']="CREATE TABLE picture_selections (
+  id int(11) NOT NULL auto_increment,
+  user_id int(11) default NULL,
+  picture_id int(11) default NULL,
+  created_at datetime default NULL,
+  PRIMARY KEY  (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+	$createtable['pictures']="CREATE TABLE pictures (
+  id int(11) NOT NULL auto_increment,
+  `data` mediumblob,
+  user_id int(11) default NULL,
+  PRIMARY KEY  (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+	$createtable['policies']="CREATE TABLE policies (
+  id int(11) NOT NULL auto_increment,
+  contributor_id int(11) default NULL,
+  contributor_type varchar(255) default NULL,
+  `name` varchar(255) default NULL,
+  download_public tinyint(1) default '1',
+  edit_public tinyint(1) default '1',
+  view_public tinyint(1) default '1',
+  download_protected tinyint(1) default '1',
+  edit_protected tinyint(1) default '1',
+  view_protected tinyint(1) default '1',
+  created_at datetime default NULL,
+  updated_at datetime default NULL,
+  share_mode int(11) default NULL,
+  update_mode int(11) default NULL,
+  PRIMARY KEY  (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+	$createtable['profiles']="CREATE TABLE profiles (
+  id int(11) NOT NULL auto_increment,
+  user_id int(11) default NULL,
+  picture_id int(11) default NULL,
+  email varchar(255) default NULL,
+  website varchar(255) default NULL,
+  created_at datetime default NULL,
+  updated_at datetime default NULL,
+  body text,
+  body_html text,
+  field_or_industry varchar(255) default NULL,
+  occupation_or_roles varchar(255) default NULL,
+  organisations text,
+  location_city varchar(255) default NULL,
+  location_country varchar(255) default NULL,
+  interests text,
+  contact_details text,
+  PRIMARY KEY  (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+	$createtable['ratings']="CREATE TABLE ratings (
+  id int(11) NOT NULL auto_increment,
+  rating int(11) default '0',
+  created_at datetime NOT NULL default '0000-00-00 00:00:00',
+  rateable_type varchar(15) NOT NULL default '',
+  rateable_id int(11) NOT NULL default '0',
+  user_id int(11) NOT NULL default '0',
+  PRIMARY KEY  (id),
+  KEY fk_ratings_user (user_id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+	$createtable['relationships']="CREATE TABLE relationships (
+  id int(11) NOT NULL auto_increment,
+  network_id int(11) default NULL,
+  relation_id int(11) default NULL,
+  created_at datetime default NULL,
+  accepted_at datetime default NULL,
+  PRIMARY KEY  (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
 	$createtable['remote_workflows']="CREATE TABLE remote_workflows (
   id int(11) NOT NULL auto_increment,
   workflow_id int(11) default NULL,
@@ -168,12 +497,12 @@
 	$publicsql['profiles']="select * from profiles";
         $publicsql['ratings']="select ratings.* from ratings inner join contributions on ratings.rateable_id=contributions.contributable_id and ratings.rateable_type=contributions.contributable_type inner join policies on contributions.policy_id=policies.id where policies.view_public=1";
         $publicsql['relationships']="select * from relationships";
-	$publicsql['remote_workflows']="select remote_workflows.* from remote_workflows inner join contributions on remote_workflows.workflow_id=contributions.contributable_id and contributions.contributable_type='Workflow' inner join policies on contributions.policy_id=policies.id where policies.view_public=1";
+	$publicsql['remote_workflows']="select remote_workflows.* from remote_workflows inner join contributions on remote_workflows.workflow_id=contributions.contributable_id and contributions.contributable_type='Workflow' inner join policies on contributions.policy_id=policies.id where policies.view_public=1 and 1=0";
 
         $publicsql['reviews']="select reviews.* from reviews inner join contributions on reviews.reviewable_id=contributions.contributable_id and reviews.reviewable_type=contributions.contributable_type inner join policies on contributions.policy_id=policies.id where policies.view_public=1";
-        $publicsql['taverna_enactors']="select * from taverna_enactors";
+        $publicsql['taverna_enactors']="select * from taverna_enactors where 1=0";
         $publicsql['taggings']="select taggings.* from taggings left join contributions on taggings.taggable_id=contributions.contributable_id and taggings.taggable_type=contributions.contributable_type left join policies on contributions.policy_id=policies.id where policies.view_public=1 or taggings.taggable_type='Network'";
-        $publicsql['users']="select id, openid_url, name, created_at, updated_at, last_seen_at, username, downloads_count, viewings_count, email, unconfirmed_email, activated_at, receive_notifications from users";
+        $publicsql['users']="select id, openid_url, name, created_at, updated_at, last_seen_at, username, downloads_count, viewings_count, email, unconfirmed_email, email_confirmed_at, activated_at, receive_notifications from users";
 	$publicsql['viewings']="select viewings.* from viewings inner join contributions on viewings.contribution_id=contributions.id inner join policies on contributions.policy_id=policies.id where policies.view_public=1";        
 	$publicsql['workflows']="select workflows.* from contributions inner join workflows on contributions.contributable_id=workflows.id inner join policies on contributions.policy_id=policies.id where contributable_type='Workflow' and policies.view_public=1";
         $publicsql['workflow_versions']="select workflow_versions.* from contributions inner join workflows on contributions.contributable_id=workflows.id inner join workflow_versions on workflows.id=workflow_versions.workflow_id inner join policies on contributions.policy_id=policies.id where contributable_type='Workflow' and policies.view_public=1";
