1400c8f20e11034016e130606bfd6b34feadade4
Sergejs Aleksejevs <alekses6@cs.man.ac.uk>
2008-11-27 15:29:27 +0000

    Throttling. Created model / migration.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/branches/invitation_throttling@2009 4f7b4649-9ca9-420a-81b3-55a188e4d46f

---
+++ app/models/activity_limit.rb
@@ -0,0 +1,2 @@
+class ActivityLimit < ActiveRecord::Base
+end
---
+++ test/unit/activity_limit_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class ActivityLimitTest < Test::Unit::TestCase
+  fixtures :activity_limits
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
