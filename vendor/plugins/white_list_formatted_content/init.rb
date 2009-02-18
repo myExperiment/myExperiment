4aba9bb6488361ed191f115febee2d57b3218bd8
Don Cruickshank <dgc@ecs.soton.ac.uk>
2009-02-18 16:50:25 +0000

    added plain text description renderer and removed double white_list cases
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/trunk@2109 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- vendor/plugins/white_list_formatted_content/init.rb
+++ vendor/plugins/white_list_formatted_content/init.rb
@@ -13,13 +13,62 @@ ActiveRecord::Base.class_eval do
   end
 
   protected
+
+  def render_plain_text_to_html(text)
+
+    # separate paragraphs that end with a colon
+
+    text.gsub!(/:\n/, ":\n\n")
+    
+    # fold indented parts of bulleted lines
+
+    text.gsub!(/^\* .*\n(  .*\n)+/) { |b|
+      b.gsub(/\n/, " ") + "\n"
+    }
+
+    # fold indented parts of numbered lines
+
+    text.gsub!(/^[0-9]+\. .*\n(   .*\n)+/) { |b|
+      b.gsub(/\n/, " ") + "\n"
+    }
+
+    # convert each adjacent set of bulleted lines into HTML
+
+    text.gsub!(/^\* .*\n(\* .*\n|\s*\n)*/) { |b|
+      b.gsub!(/^\s*\n/, "")
+      "\n<ul>\n" + b.gsub(/^\* (.*)/, '<li>\1</li>') + "</ul>\n\n"
+    }
+
+    # convert each adjacent set of numbered lines into HTML
+
+    text.gsub!(/^[0-9]+\. .*\n(([0-9]+\. .*\n)|\s*\n)*/) { |b|
+      b.gsub!(/^\s*\n/, "")
+      "\n<ol>\n" + b.gsub(/^([0-9]+)\. (.*)/, '<li value="\1">\2</li>') + "</ol>\n\n"
+    }
+
+    # make sure the last line is terminated
+
+    text = "#{text}\n"
+
+    # fold multiple adjacent blank lines
+
+    text.gsub!(/^\s*\n(\s*\n)*/, "\n")
+
+    # place each section in an HTML paragraph
+
+    text.gsub!(/((^.*\S+.*\n)+)/, "<p>\n\\1</p>\n")
+
+    text
+  end
+
+
     def format_content
       body.strip! if body.respond_to?(:strip!)
       self.body_html = body.blank? ? '' : body_html_with_formatting
     end
     
     def body_html_with_formatting
-      body_html = auto_link body { |text| truncate(text, 50) }
+      body_html = auto_link(body.starts_with?('<') ? body : render_plain_text_to_html(body))
       white_list(body_html)
     end
 end
