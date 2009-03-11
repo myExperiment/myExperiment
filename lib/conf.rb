764e8c27ef4f9a2e4295f5e851e7593073390aeb
Don Cruickshank <dgc@ecs.soton.ac.uk>
2009-03-11 13:45:16 +0000

    added the missing Conf class
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/trunk@2117 4f7b4649-9ca9-420a-81b3-55a188e4d46f

---
+++ lib/conf.rb
@@ -0,0 +1,49 @@
+# myExperiment: lib/conf.rb
+# 
+# Copyright (c) 2009 University of Manchester and the University of Southampton.
+# See license.txt for details.
+
+# Configuration module
+
+require 'yaml'
+
+class Conf
+
+  @vars = YAML::load_file("config/settings.yml")
+
+  def self.sitename
+    @vars['site_name']
+  end
+
+  def self.site_logo
+    @vars['site_logo']
+  end
+
+  def self.notifications_email_address
+    @vars['notifications_email_address']
+  end
+
+  def self.feedback_email_address
+    @vars['feedback_email_address']
+  end
+
+  def self.base_uri
+    @vars['base_uri']
+  end
+
+  def self.admins
+    @vars['admins']
+  end
+
+  def self.main_tabs
+    @vars['main_tabs']
+  end
+
+  # This method is required to create an administrator in the test fixtures
+
+  def self.admins=(value)
+    @vars['admins'] = value
+  end
+
+end
+
