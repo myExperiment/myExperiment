71b8c580d015efe4ae49916bafdef6165a19a390
Don Cruickshank <dgc@ecs.soton.ac.uk>
2009-03-27 10:55:26 +0000

    added more enactment categories
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/trunk@2141 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- lib/authorization.rb
+++ lib/authorization.rb
@@ -296,7 +296,7 @@ module Authorization
         action = 'view'
       when 'edit', 'new', 'create', 'update', 'new_version', 'create_version', 'destroy_version', 'edit_version', 'update_version', 'new_item', 'create_item', 'edit_item', 'update_item', 'quick_add', 'resolve_link', 'process_tag_suggestions'
         action = 'edit'
-      when 'download', 'named_download', 'launch', 'submit_job', 'save_inputs', 'refresh_status', 'rerun', 'refresh_outputs'
+      when 'download', 'named_download', 'launch', 'submit_job', 'save_inputs', 'refresh_status', 'rerun', 'refresh_outputs', 'render_output', 'outputs_xml', 'outputs_package'
         action = 'download'
       when 'destroy', 'delete', 'destroy_item'
         action = 'destroy'
