9f3d171f91cefc2e1da2b72f3829a42f5cac6084
Don Cruickshank <dgc@ecs.soton.ac.uk>
2009-05-27 14:53:43 +0000

    limited ability to favourite an object more than once
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/trunk@2201 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -16,6 +16,8 @@ class Bookmark < ActiveRecord::Base
   validates_presence_of :bookmarkable
   validates_presence_of :user
 
+  validate :check_duplicate_favourites
+
   # Helper class method to lookup all comments assigned
   # to all commentable types for a given user.
   def self.find_bookmarks_by_user(user)
@@ -29,5 +31,11 @@ class Bookmark < ActiveRecord::Base
   # given the commentable class name and id 
   def self.find_bookmarkable(commentable_str, commentable_id)
     commentable_str.constantize.find(commentable_id)
+  end
+
+  def check_duplicate_favourites
+    if Bookmark.find_by_user_id_and_bookmarkable_type_and_bookmarkable_id(user_id, bookmarkable_type, bookmarkable_id)
+      errors.add_to_base("Objects cannot be favourited more than once")
+    end
   end
 end
