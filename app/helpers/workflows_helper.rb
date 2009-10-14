c2e77b74e91dfd3d988b046c57dc469ba1731368
Don Cruickshank <dgc@ecs.soton.ac.uk>
2009-10-14 11:46:11 +0000

    added COinS to workflows
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/trunk@2284 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- app/helpers/workflows_helper.rb
+++ app/helpers/workflows_helper.rb
@@ -3,6 +3,8 @@
 # Copyright (c) 2007 University of Manchester and the University of Southampton.
 # See license.txt for details.
 
+require 'openurl'
+
 module WorkflowsHelper
   
   def workflow_types
@@ -16,4 +18,15 @@ module WorkflowsHelper
     return (klass.nil? ? "other" : h(klass.to_s.demodulize.underscore))
   end
   
+  def workflow_context_object(workflow)
+
+    co = OpenURL::ContextObject.new
+
+    co.referent.set_metadata('title', workflow.title)
+    co.referent.set_metadata('date', workflow.created_at.strftime("%Y-%m-%d"))
+    co.referent.set_metadata('au', workflow.contributor.name)
+
+    co.kev
+  end
+
 end
