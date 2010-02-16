601d3e814fd640be02dc2d321980c4116a49019d
Don Cruickshank <dgc@ecs.soton.ac.uk>
2010-02-16 16:24:02 +0000

    changed user tab style to match rest of website
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/trunk@2335 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- vendor/plugins/widgets/lib/widgets/tabnav_helper.rb
+++ vendor/plugins/widgets/lib/widgets/tabnav_helper.rb
@@ -69,6 +69,8 @@ module Widgets
     def render_tabnav_tabs
       out tag('ul', {} , true)
     
+      offset = 0
+
       @_tabnav.tabs.each do |tab|      
         li_options = {}
         li_options[:id] = "#{tab.html[:id]}_container" if tab.html[:id] 
@@ -80,6 +82,9 @@ module Widgets
           li_options[:class] = "active" 
         end
                 
+        li_options[:style] = "position: relative; left: #{offset}px"
+        offset -= 1
+
         out tag('li', li_options, true)
         if tab.disabled? || (tab.link.empty? && tab.remote_link.nil?)
           out content_tag('span', tab.name, tab.html) 
@@ -100,7 +105,7 @@ module Widgets
         else
           raise "WHAT THE HELL?"
         end 
-        out "</li> \n"
+        out "</li>"
       end 
       out '</ul>'
     end  
