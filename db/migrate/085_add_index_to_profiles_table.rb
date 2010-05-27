6e18a2df52a1eb0a85bda4ee8b637c7aa2b37338
Danius Michaelides <dtm@ecs.soton.ac.uk>
2010-05-27 13:19:28 +0000

    Let rails sort out the name.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/trunk@2420 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- db/migrate/085_add_index_to_profiles_table.rb
+++ db/migrate/085_add_index_to_profiles_table.rb
@@ -1,13 +1,13 @@
 class AddIndexToProfilesTable < ActiveRecord::Migration
   def self.up
 
-    add_index :profiles, ["user_id"], :name => "index_profiles_on_user_id"
+    add_index :profiles, ["user_id"]
 
   end
 
   def self.down
 
-    remove_index :profiles, :name => "index_profiles_on_user_id"
+    remove_index :profiles, ["user_id"]
 
   end
 end
