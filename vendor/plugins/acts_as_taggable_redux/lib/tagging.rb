4aa464f0df16d5278ecc347a1293b5d9c06c8b4a
Don Cruickshank <dgc@ecs.soton.ac.uk>
2011-03-17 13:47:11 +0000

    added a label accessor for taggings and added validations
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/trunk@2579 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- vendor/plugins/acts_as_taggable_redux/lib/tagging.rb
+++ vendor/plugins/acts_as_taggable_redux/lib/tagging.rb
@@ -1,8 +1,19 @@
 class Tagging < ActiveRecord::Base
+
   belongs_to :tag, :counter_cache => true
   belongs_to :taggable, :polymorphic => true
   belongs_to :user
-  
+
+  validates_presence_of :tag
+  validates_presence_of :user
+  validates_presence_of :taggable
+
+  def before_validation
+    if taggable
+      return false unless taggable.respond_to?('add_tag')
+    end
+  end
+
   # returns the 'last created' Taggings
   # the maximum number of results is set by #limit#
   def self.latest(limit=10)
@@ -14,4 +25,10 @@ class Tagging < ActiveRecord::Base
   def label
     return tag.name if tag
   end
+
+  def label=(label)
+    self.tag = Tag.find_by_name(label)    if self.tag.nil?
+    self.tag = Tag.create(:name => label) if self.tag.nil?
+  end
 end
+
