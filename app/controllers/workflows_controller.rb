59e51d9ba6eab5d7207b6789139e3c536bf103f4
Danius Michaelides <dtm@ecs.soton.ac.uk>
2011-09-06 08:24:32 +0000

    Fix for uploading previews which was failing.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/trunk@2687 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- app/controllers/workflows_controller.rb
+++ app/controllers/workflows_controller.rb
@@ -978,7 +978,7 @@ private
     # The dependency on file_column has been removed, but this code remains
     # disabled on Windows until it is confirmed as working.
     unless RUBY_PLATFORM =~ /mswin32/
-      workflow_to_set.image = params[:workflow][:preview] unless params[:workflow][:preview].empty?
+      workflow_to_set.image = params[:workflow][:preview].read unless params[:workflow][:preview].stat.size<1
     end
     
     # Set the internal unique name for this particular workflow (or workflow_version).
