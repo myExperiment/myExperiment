fc39a10215b18f7c8180b57b814b641652ba014e
Don Cruickshank <dgc@ecs.soton.ac.uk>
2011-09-13 15:34:47 +0000

    added public_view and public_download to policies
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/trunk@2700 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- app/models/policy.rb
+++ app/models/policy.rb
@@ -17,12 +17,24 @@ class Policy < ActiveRecord::Base
   
   validates_presence_of :contributor, :name
   
+  before_save do |policy|
+
+    if policy.share_mode
+      policy.public_view     = policy.share_mode < 3
+      policy.public_download = policy.share_mode < 1
+    end
+
+    true
+  end
+
   # THIS IS THE DEFAULT POLICY (see /app/views/policies/_list_form.rhtml)
   def self._default(c_utor, c_ution=nil)
     rtn = Policy.new(:name => "A default policy",  # "anyone can view and download and no one else can edit"
                      :contributor => c_utor,
                      :share_mode => 0,
-                     :update_mode => 6)     
+                     :update_mode => 6,
+                     :public_view => false,
+                     :public_download => false)     
                      
     c_ution.policy = rtn unless c_ution.nil?
     
@@ -36,6 +48,8 @@ class Policy < ActiveRecord::Base
     self.contributor = other.contributor
     self.share_mode = other.share_mode
     self.update_mode = other.update_mode
+    self.public_view = other.public_view
+    self.public_download = other.public_download
   end
   
   
