d290ca6e38fc8bd497feb7ffa71149320be8e703
David Newman <drn@ecs.soton.ac.uk>
2011-11-07 17:07:04 +0000

    Fixed incompatibilities when using ruby 1.8.7 with boot.rb and the truncate method
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/trunk@2788 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- config/environment.rb
+++ config/environment.rb
@@ -112,3 +112,15 @@ end
 
 load 'config/environment_private.rb' if FileTest.exist?('config/environment_private.rb')
 
+# Fix for Ruby 1.8.7 when using Rails < 2.2
+module ActionView
+  module Helpers
+    module TextHelper
+      def truncate(text, length = 30, truncate_string = "...")
+        if text.nil? then return end
+        l = length - truncate_string.chars.to_a.size
+        (text.chars.to_a.size > length ? text.chars.to_a[0...l].join + truncate_string : text).to_s
+      end
+    end
+  end
+end
