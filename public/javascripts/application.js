4d322f4b72426e43bf10252a9d9d2fd763b17a27
Danius Michaelides <dtm@ecs.soton.ac.uk>
2012-01-20 10:33:48 +0000

    Reinstate missing javascript functions that were deleted in rails2 port. These are the only functions that seem to be in use.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/trunk@2910 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,22 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function redirectToAddToPack() {
+  var url = $("add_to_pack_selection").value;
+  location.href = url;
+}
+
+function checkAll(checkboxes){
+  for (var i=0; i<checkboxes.length; i++){
+    checkbox = document.getElementById(checkboxes[i]);
+    checkbox.checked="checked";
+  }
+}
+
+function uncheckAll(checkboxes){
+  for (var i=0; i<checkboxes.length; i++){
+    checkbox = document.getElementById(checkboxes[i]);
+    checkbox.checked="";
+  }
+}
+
