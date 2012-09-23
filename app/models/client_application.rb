df31e5f7b67c1757533799f2c6775c6cfcf3a9da
Don Cruickshank <dgc@ecs.soton.ac.uk>
2012-09-23 05:27:41 +0000

    changed webapp code to match oauth templates for oauth-provider 0.4.1
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/branches/wf4ever@3131 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- app/models/client_application.rb
+++ app/models/client_application.rb
@@ -1,54 +1,60 @@
 require 'oauth'
 class ClientApplication < ActiveRecord::Base
   belongs_to :user
-  has_many :tokens,:class_name=>"OauthToken", :dependent => :destroy
+  has_many :tokens, :class_name => "OauthToken", :dependent => :destroy
+  has_many :access_tokens
+  has_many :oauth2_verifiers
+  has_many :oauth_tokens
   has_many :permissions,
            :class_name => "KeyPermission",
            :order => "key_permissions.for",
            :dependent => :destroy
   belongs_to :creator,
              :class_name => "User",
-	     :foreign_key => "creator_id"
-  validates_presence_of :name,:url,:key,:secret
+             :foreign_key => "creator_id"
+  validates_presence_of :name, :url, :key, :secret
   validates_uniqueness_of :key
   before_validation_on_create :generate_keys
-  
+
+  validates_format_of :url, :with => /\Ahttp(s?):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/i
+  validates_format_of :support_url, :with => /\Ahttp(s?):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/i, :allow_blank=>true
+  validates_format_of :callback_url, :with => /\Ahttp(s?):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/i, :allow_blank=>true
+
+  attr_accessor :token_callback_url
+
   def self.find_token(token_key)
-    token=OauthToken.find_by_token(token_key, :include => :client_application)
-    logger.info "Loaded #{token.token} which was authorized by (user_id=#{token.user_id}) on the #{token.authorized_at}"
-    return token if token.authorized?
-    nil
-  end
-  
-  def self.verify_request(request, options = {}, &block)
-    begin
-      signature=OAuth::Signature.build(request,options,&block)
-      logger.info "Signature Base String: #{signature.signature_base_string}"
-      logger.info "Consumer: #{signature.send :consumer_key}"
-      logger.info "Token: #{signature.send :token}"
-      return false unless OauthNonce.remember(signature.request.nonce,signature.request.timestamp)
-      value=signature.verify
-#      value=true
-      logger.info "Signature verification returned: #{value.to_s}"
-      value
-    rescue OAuth::Signature::UnknownSignatureMethod=>e
-      #logger.info "ERROR"+e.to_s
-     false
+    token = OauthToken.find_by_token(token_key, :include => :client_application)
+    if token && token.authorized?
+      token
+    else
+      nil
     end
   end
-  
+
+  def self.verify_request(request, options = {}, &block)
+    begin
+      signature = OAuth::Signature.build(request, options, &block)
+      return false unless OauthNonce.remember(signature.request.nonce, signature.request.timestamp)
+      value = signature.verify
+      value
+    rescue OAuth::Signature::UnknownSignatureMethod => e
+      false
+    end
+  end
+
   def oauth_server
-    @oauth_server||=OAuth::Server.new "http://your.site"
+    @oauth_server ||= OAuth::Server.new("http://your.site")
   end
-  
+
   def credentials
-    @oauth_client||=OAuth::Consumer.new key,secret
+    @oauth_client ||= OAuth::Consumer.new(key, secret)
   end
-    
-  def create_request_token
-    RequestToken.create :client_application=>self
+
+  # If your application requires passing in extra parameters handle it here
+  def create_request_token(params={})
+    RequestToken.create :client_application => self, :callback_url=>self.token_callback_url
   end
-  
+
   def permissions_for
     permissions_for= []
     for key_permission in self.permissions do
@@ -56,12 +62,11 @@ class ClientApplication < ActiveRecord::Base
     end
     permissions_for
   end
-  
-  protected
-  
+
+protected
+
   def generate_keys
-    @oauth_client=oauth_server.generate_consumer_credentials
-    self.key=@oauth_client.key
-    self.secret=@oauth_client.secret
+    self.key = OAuth::Helper.generate_key(40)[0,40]
+    self.secret = OAuth::Helper.generate_key(40)[0,40]
   end
 end
