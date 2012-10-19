9282c0915f5f4257b6a41cc502dad32583dda8d5
Don Cruickshank <dgc@ecs.soton.ac.uk>
2012-10-19 09:45:57 +0000

    removed pre rails 2.2 workaround now that we're on rails 2.3.14
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/trunk@3165 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- config/environment.rb
+++ config/environment.rb
@@ -106,17 +106,3 @@ end
 
 load 'config/environment_private.rb' if FileTest.exist?('config/environment_private.rb')
 
-# Fix for Ruby 1.8.7 when using Rails < 2.2
-if RUBY_VERSION == "1.8.7" && ((Rails::VERSION::MAJOR == 1) || (Rails::VERSION::MAJOR == 2 && Rails::VERSION::MINOR < 2))
-  module ActionView
-    module Helpers
-      module TextHelper
-        def truncate(text, length = 30, truncate_string = "...")
-          if text.nil? then return end
-          l = length - truncate_string.chars.to_a.size
-          (text.chars.to_a.size > length ? text.chars.to_a[0...l].join + truncate_string : text).to_s
-        end
-      end
-    end
-  end
-end
