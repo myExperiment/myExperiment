95d39a501a37a5d3ab6522d7b0a517470fb2ea8f
Finn Bacall <finn.bacall@manchester.ac.uk>
2012-11-07 11:49:35 +0000

    Fixed minor wsdl deprecation issue
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/trunk@3180 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- app/models/wsdl_deprecation.rb
+++ app/models/wsdl_deprecation.rb
@@ -3,7 +3,7 @@ class WsdlDeprecation < ActiveRecord::Base
   validates_uniqueness_of :wsdl
 
   def affected_workflows
-    WorkflowProcessor.find_all_by_wsdl(wsdl, :include => :workflow).uniq.compact
+    WorkflowProcessor.find_all_by_wsdl(wsdl, :include => :workflow).map {|wp| wp.workflow}.uniq.compact
   end
 
 end
