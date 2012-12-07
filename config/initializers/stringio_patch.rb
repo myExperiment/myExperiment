da05339a1eebcebef361fcfb9168e24e2c8050b8
Don Cruickshank <dgc@ecs.soton.ac.uk>
2012-12-07 10:32:30 +0000

    copied patch for read_partial issue to trunk
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/trunk@3241 4f7b4649-9ca9-420a-81b3-55a188e4d46f

---
+++ config/initializers/stringio_patch.rb
@@ -0,0 +1,18 @@
+# myExperiment: config/initializers/stringio_patch.rb
+#
+# Copyright (c) 2012 University of Manchester and the University of Southampton.
+# See license.txt for details.
+
+if VERSION < "1.9.1"
+  class StringIO
+    def readpartial(*args)
+      result = read(*args)
+      if result.nil?
+        raise EOFError
+      else
+        result
+      end
+    end
+  end
+end
+
