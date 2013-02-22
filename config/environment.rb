863a38ad0ac9142a80da261b7361380c0617c3b4
Don Cruickshank <dgc@ecs.soton.ac.uk>
2013-02-22 09:35:00 +0000

    missed a change in r3431
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/trunk@3434 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ RAILS_GEM_VERSION = '2.3.17' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
-require 'lib/conf'
+require 'conf'
 require 'uri'
 
 Rails::Initializer.run do |config|
