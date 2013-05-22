f549687e0df4bc848b08f219bf5f0435d359cd37
Don Cruickshank <dgc@ecs.soton.ac.uk>
2013-05-22 13:42:17 +0000

    added size to content blobs
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/branches/packs@3558 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- app/models/content_blob.rb
+++ app/models/content_blob.rb
@@ -1,12 +1,18 @@
 # myExperiment: app/models/content_blob.rb
 #
-# Copyright (c) 2009 University of Manchester and the University of Southampton.
-# See license.txt for details.
+# Copyright (c) 2007-2013 The University of Manchester, the University of
+# Oxford, and the University of Southampton.  See license.txt for details.
 
 require 'digest/md5'
 require 'digest/sha1'
 
 class ContentBlob < ActiveRecord::Base
+
+  before_save :update_metadata
+
+  # validates_presence_of uses a function that assumes UTF-8 encoding and thus
+  # has issues with other encodings.  The following validation provides similar
+  # functionality to validates_presence_of on the content blob data.
 
   validate do |record|
     if record.data.nil? || record.data.length == 0
@@ -14,13 +20,16 @@ class ContentBlob < ActiveRecord::Base
     end
   end
 
-  before_save do |blob|
-    blob.update_checksums
-  end
+  def update_metadata
 
-  def update_checksums
     self.md5  = Digest::MD5.hexdigest(data)
     self.sha1 = Digest::SHA1.hexdigest(data)
-  end
 
+    case self.data
+    when StringIO
+      self.size = self.data.size
+    when String
+      self.size = self.data.bytesize
+    end
+  end
 end
