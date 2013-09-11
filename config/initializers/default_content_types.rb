77ff4281961c4c15ecd5fb6043428599d344f20c
Finn Bacall <finn.bacall@manchester.ac.uk>
2013-09-11 11:13:15 +0000

    Fixed issue with default_content_types preventing migrations on a new db
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/trunk@3704 4f7b4649-9ca9-420a-81b3-55a188e4d46f

--- config/initializers/default_content_types.rb
+++ config/initializers/default_content_types.rb
@@ -5,14 +5,16 @@
 
 # Loads a set default content_types into the database
 
-content_types = YAML::load_file("config/content_types.yml")
+if ActiveRecord::Base.connection.table_exists?('content_types')
+  content_types = YAML::load_file("config/content_types.yml")
 
-Rails.logger.debug('Loading content types...')
-ContentType.transaction do
-  content_types.each do |k,v|
-    unless ContentType.find_by_title_and_mime_type_and_category(v['title'], v['mime_type'], v['category'])
-      Rails.logger.debug("\tCreating content type #{v['title']}: #{v['mime_type']}")
-      ContentType.create(v)
+  Rails.logger.debug('Loading content types...')
+  ContentType.transaction do
+    content_types.each do |k,v|
+      unless ContentType.find_by_title_and_mime_type_and_category(v['title'], v['mime_type'], v['category'])
+        Rails.logger.debug("\tCreating content type #{v['title']}: #{v['mime_type']}")
+        ContentType.create(v)
+      end
     end
   end
 end
