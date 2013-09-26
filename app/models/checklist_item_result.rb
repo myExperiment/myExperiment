c782397a4488935f132b38283c046edc4e7fb819
Don Cruickshank <dgc@ecs.soton.ac.uk>
2013-09-26 02:04:01 +0000

    added model files for checklist results
    git-svn-id: svn+ssh://rubyforge.org/var/svn/myexperiment/branches/packs@3722 4f7b4649-9ca9-420a-81b3-55a188e4d46f

---
+++ app/models/checklist_item_result.rb
@@ -0,0 +1,11 @@
+# myExperiment: app/models/checklist_item_result.rb
+#
+# Copyright (c) 2007-2013 The University of Manchester, the University of
+# Oxford, and the University of Southampton.  See license.txt for details.
+
+class ChecklistItemResult < ActiveRecord::Base
+
+  belongs_to :checklist_result
+
+end
+
