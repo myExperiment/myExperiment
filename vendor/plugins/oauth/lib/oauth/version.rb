4d96e4e89b8cafdbeee57961002c8963dfc5e407
Finn Bacall <finn.bacall@manchester.ac.uk>
2014-06-30 14:58:34 +0100

    Replaced oauth plugin with gem

--- vendor/plugins/oauth/lib/oauth/version.rb
+++
@@ -1,9 +0,0 @@
-module Oauth #:nodoc:
-  module VERSION #:nodoc:
-    MAJOR = 0
-    MINOR = 2
-    TINY  = 4
-
-    STRING = [MAJOR, MINOR, TINY].join('.')
-  end
-end
--- vendor/plugins/oauth/test/test_helper.rb
+++
@@ -1,7 +0,0 @@
-require 'test/unit'
-require File.dirname(__FILE__) + '/../lib/oauth'
-
-def requests(request)
-  Marshal.load(File.read(File.dirname(__FILE__) + '/fixtures/' + request))
-end
-
