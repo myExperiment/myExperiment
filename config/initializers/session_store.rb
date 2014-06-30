04426a7e2c8da71d7861e6643e8268f123f57fb8
Finn Bacall <finn.bacall@manchester.ac.uk>
2014-06-30 15:16:12 +0100

    Fixed session store initializer

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,12 +4,8 @@
 # If you change this key, all old sessions will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => 'myexperiment_session',
-  :secret      => Conf.session_store_secret
-}
 
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-ActionController::Base.session_store = :active_record_store
+MyExperiment::Application.config.session_store(:active_record_store, {
+    :key => 'myexperiment_session',
+    :secret => Conf.session_store_secret
+})
