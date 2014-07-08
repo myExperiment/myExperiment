a2de2d40b92460cbe4f5c1a990f17afbe54956de
Finn Bacall <finn.bacall@manchester.ac.uk>
2014-07-08 12:04:06 +0100

    Fixed stupid copy & paste error in error_messages_for

--- app/helpers/deprecated_methods_helper.rb
+++ app/helpers/deprecated_methods_helper.rb
@@ -1,11 +1,11 @@
 module DeprecatedMethodsHelper
 #TODO: Inline me?
   def error_messages_for(obj)
-    object = eval("@#{obj.to_s}")
+    object = instance_variable_get("@#{obj}")
 
     if object.errors.any?
       content_tag(:ul) do
-        @post.errors.full_messages.each do |msg|
+        object.errors.full_messages.each do |msg|
           content_tag(:li) do
             msg
           end
