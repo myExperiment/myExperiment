ecf70a7838cb473cd0c0bade77d5077d89a53f9b
Finn Bacall <finn.bacall@manchester.ac.uk>
2014-11-10 09:22:26 +0000

    Memoize commit

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,1 +1,13 @@
-$commit = `git log -n 1`.split("\n")[0].split[1]
+module MyExperiment
+
+  class Version
+
+    def self.commit
+      $commit ||= `git log -n 1`.split("\n")[0].split[1]
+    end
+
+  end
+
+end
+
+
