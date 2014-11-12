d5bd7d8c3714bef9bb6f3eb622ed4b82205c9f82
Finn Bacall <finn.bacall@manchester.ac.uk>
2014-11-12 14:06:52 +0000

    Updated Delayed::Job#enqueue call to match new API

--- config/initializers/service_deprecation_watcher.rb
+++ config/initializers/service_deprecation_watcher.rb
@@ -7,6 +7,6 @@ require 'service_deprecation_job'
 
 if ActiveRecord::Base.connection.table_exists?('delayed_jobs')
   unless Delayed::Job.exists?(:handler => ServiceDeprecationJob.new.to_yaml)
-    Delayed::Job.enqueue(ServiceDeprecationJob.new, 1, 5.minutes.from_now)
+    Delayed::Job.enqueue(ServiceDeprecationJob.new, :priority => 1, :run_at => 5.minutes.from_now)
   end
 end
